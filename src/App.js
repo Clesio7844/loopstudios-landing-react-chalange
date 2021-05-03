8b8b1319064f3651977e7d03a283bbd684b776b6
david <daviddecastro42@live.co.uk>
2021-05-03 15:08:46 +0100

    finished the header component mobile section amd showcase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Showcase from './components/Showcase';
 
-function App() {
-  return <div className='App'>LoopStudio</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Showcase />
+    </>
+  );
+};
 
 export default App;
