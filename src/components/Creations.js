6ed56743f6a93538eb34edae4ccbde7f4659760d
david <daviddecastro42@live.co.uk>
2021-05-04 01:08:50 +0100

    finished on creatdion component and use data from data-mobile file

--- src/components/Creations.js
+++ src/components/Creations.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from '../data-mobile';
 
 const Creations = () => {
+  const [cards, setCards] = useState(data);
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <section className='creation-mobile'>
+      <h2>Our creations</h2>
+      <section>
+        {cards.map(card => {
+          const { id, image, title } = card;
+
+          return (
+            <article key={id}>
+              <div>
+                <img src={image} alt='creations card' />
+              </div>
+              <h3>{title}</h3>
+            </article>
+          );
+        })}
+      </section>
+      <div className='btn-container'>
+        <button className='btn'>See All</button>
+      </div>
+    </section>
   );
 };
 
---
+++ src/data-desktop.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    image: '/images/desktop/image-deep-earth.jpg',
+    title: 'Deep Fearth'
+  },
+  {
+    id: 2,
+    image: '/images/desktop/image-night-arcade.jpg',
+    title: 'Night Arcade'
+  },
+  {
+    id: 3,
+    image: '/images/desktop/image-soccer-team.jpg',
+    title: 'Soccer Team vr'
+  },
+  {
+    id: 4,
+    image: '/public/images/desktop/image-grid.jpg',
+    title: 'The Grid'
+  },
+  {
+    id: 5,
+    image: '/images/desktop/image-from-above.jpg',
+    title: 'From up Above vr'
+  },
+  {
+    id: 6,
+    image: '/public/images/desktop/image-pocket-borealis.jpg',
+    title: 'Pocket Boreals'
+  },
+  {
+    id: 7,
+    image: '/images/desktop/image-curiosity.jpg',
+    title: 'The Curiosity'
+  },
+  {
+    id: 8,
+    image: '/images/desktop/image-fisheye.jpg',
+    title: 'Make it Fishye'
+  }
+];
+
+export default data;
